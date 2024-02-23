a78918967d70af1f2de5191f2e02e89a840e4a72
Muteeb Bhat <muteebhat7@gmail.com>
2024-02-23 15:45:30 +0530

    -variables -datatypes

---
+++ datatypes.js
@@ -0,0 +1,10 @@
+let name = "Muteeb" //string
+let age = "17" //number
+let isActive = true  //Bool
+
+console.log(name)
+console.log(age)
+console.log(isActive)
+
+
+console.log(typeof name)
---
+++ variables.js
@@ -0,0 +1,11 @@
+const accountId = 123344
+
+let accountEmail = "muteeb@gmail.com"
+var accountPassword = "1234"
+
+accountCity = "Delhi"
+
+
+
+console.log(accountCity);
+console.table([accountId, accountEmail, accountPassword]);
