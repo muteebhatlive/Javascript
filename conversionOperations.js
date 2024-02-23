b759455b4a00a1e59888b8b5d60579a6614fd7d5
Muteeb Bhat <muteebhat7@gmail.com>
2024-02-23 15:45:55 +0530

    - Conversion Operations

---
+++ conversionOperations.js
@@ -0,0 +1,16 @@
+let score = "333"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+
+console.log(typeof(valueInNumber))
+
+console.log(typeof(score))
+
+
+let isLoggedIn = 1
+let boolLogged = Boolean(isLoggedIn)
+console.log(boolLogged);
+
