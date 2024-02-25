af9e19f7fe21207d85e6e9d7c653386964c148a9
Muteeb Bhat <muteebhat7@gmail.com>
2024-02-25 16:47:48 +0530

    nums and Math

---
+++ 07_numbers.js
@@ -0,0 +1,36 @@
+const score = 80
+
+const balance =  new Number(100)
+// console.log(balance);
+
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2));
+
+
+const otherNumber = 23.77323
+
+// console.log(otherNumber.toPrecision(2));
+
+
+const thousands = 10000000
+// console.log(thousands.toLocaleString('en-IN'));
+
+
+// Maths
+
+console.log(Math.abs(-5));
+console.log(Math.round(6.67));
+console.log(Math.ceil(6.67));
+console.log(Math.floor(6.67));
+
+console.log(Math.min(2,5,6,7))
+console.log(Math.max(2,5,6,7))
+
+
+console.log(Math.random())
+console.log(Math.random()*10)
+console.log(Math.round(Math.random()*10) +1 )
+
+const min = 10
+const max = 30
